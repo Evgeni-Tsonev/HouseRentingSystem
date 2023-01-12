07e30d21c64e24be24fc4ab51269d229fe32f5dc
evgeni136 <evgeni_136@abv.bg>
2023-01-12 17:38:37 +0200

    Created class Category

---
+++ HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace HouseRentingSystem.Infrastructure.Data
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public IEnumerable<House> Houses { get; set; } = new List<House>();
+    }
+}
