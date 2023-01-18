8bf1cc9f4a4a012e3e201ac54265d948ce884c5e
evgeni136 <evgeni_136@abv.bg>
2023-01-18 18:43:14 +0200

    Added custom error pages, added production environment.

--- HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
+++ HouseRentingSystem/HouseRentingSystem/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿namespace HouseRentingSystem.Controllers
 {
     using HouseRentingSystem.Core.Services.Houses;
-    using HouseRentingSystem.Models;
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
-    using System.Diagnostics;
 
     [AllowAnonymous]
     public class HomeController : BaseController
@@ -23,9 +21,19 @@
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+
+            return View(); //new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
         }
     }
 }
